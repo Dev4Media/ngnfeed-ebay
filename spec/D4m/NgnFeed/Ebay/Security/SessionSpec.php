4bf30de6a64b47962e7a92ae84fdc88fc2fbcf85
raul782 <raulrodriguez782@gmail.com>
2013-07-12 14:31:14 -0500

    Add Spec to Session and Transport

--- spec/D4m/NgnFeed/Ebay/Security/SessionSpec.php
+++ spec/D4m/NgnFeed/Ebay/Security/SessionSpec.php
@@ -5,16 +5,54 @@
  * 
  */
 
-namespace spec\D4m\NgnFeed\Ebay\Security\Session;
+namespace spec\D4m\NgnFeed\Ebay\Security;
 
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class SessionSpec extends ObjectBehavior
 {
+    const API_VERSION = 827;
+    /**
+     * @param D4m\NgnFeed\Ebay\Security\Auth\Credentials $credentials
+     * @param D4m\NgnFeed\Ebay\Serializer\SerializerDecorator $serializer
+     */
+    function let($credentials, $serializer)
+    {
+        $serializer->beADoubleOf('D4m\NgnFeed\Ebay\Serializer\SerializerDecorator');
+        $this->beConstructedWith($credentials, $serializer);
+    }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('D4m\NgnFeed\Ebay\Security\Session');
+    }
+
+    function it_should_have_a_version()
+    {
+        $this->setApiVersion(self::API_VERSION);
+        $this->getApiVersion()->shouldReturn(self::API_VERSION);
+    }
+
+    /**
+     * @param D4m\NgnFeed\Ebay\Transport\HttpClient $transport
+     * @param Guzzle\Http\Message\Response $response
+     */
+    function it_should_return_response_when_sending_a_request($transport, $response)
+    {
+
+        $apiCallName = ['X-EBAY-API-CALL-NAME' => "TestCall"];
+        $parameters = ['headers' => $apiCallName, 'request' => [] ];
+        $authType = 'api';
+
+        $transport->send(Argument::any(), Argument::any(), Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($response);
+        $this->setTransport($transport);
+        $response = $this->sendRequest($apiCallName, $parameters, $authType);
+
+        $response->shouldHaveType('Guzzle\Http\Message\Response');
+
     }
 
 
--- spec/D4m/NgnFeed/Ebay/Transport/HttpClientSpec.php
+++ spec/D4m/NgnFeed/Ebay/Transport/HttpClientSpec.php
@@ -7,7 +7,40 @@
 
 namespace spec\D4m\NgnFeed\Ebay\Transport;
 
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
-class HttpClientSpec {
+class HttpClientSpec extends ObjectBehavior
+{
+    /**
+     * @param Guzzle\Http\Client $client
+     */
+    function let($client)
+    {
+        $this->beConstructedWith($client);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('D4m\NgnFeed\Ebay\Transport\HttpClient');
+    }
+
+    /**
+     * @param Guzzle\Http\Client $client
+     * @param Guzzle\Http\Message\Request $request
+     * @param Guzzle\Http\Message\Response $response
+     */
+    function it_should_return_response_when_send($client, $request, $response)
+    {
+
+        $fakeUrl = 'https://api.sandbox.ebay.spec/ws/api.dll';
+        $body = '<xml></xml>';
+        $headers = [];
+        $request->send()->shouldBeCalled()->willReturn($response);
+        $client->post($fakeUrl, $headers, $body)->shouldBeCalled()->willReturn($request);
+
+        $this->send($fakeUrl, $headers, $body)->shouldHaveType('Guzzle\Http\Message\Response');
+
+    }
 
 }
