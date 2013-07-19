300c81c1201cf26dd89980014ccaadb9e7e59933
raul782 <raulrodriguez782@gmail.com>
2013-07-19 03:55:56 -0500

    Added HitCounterCode Enum class

---
+++ src/D4m/NgnFeed/Ebay/Model/Enum/HitCounterCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace D4m\NgnFeed\Ebay\Model\Enum;
+
+class HitCounterCode
+{
+    const NO_HIT_COUNTER = "NoHitCounter";
+    const HONESTY_STYLE = "HonestyStyle";
+    const GREEN_LED = "GreenLED";
+    const HIDDEN = "Hidden";
+    const BASIC_STYLE = "BasicStyle";
+    const RETRO_STYLE = "RetroStyle";
+    const HIDDEN_STYLE = "HiddenStyle";
+    const CUSTOM_CODE = "CustomCode";
+}
