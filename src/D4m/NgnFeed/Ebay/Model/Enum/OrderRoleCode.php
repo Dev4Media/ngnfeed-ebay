fcb1063dd5db44dbcae1be11ffb4a6454975333b
Raul Rodriguez <raulrodriguez782@gmail.com>
2014-02-24 18:19:44 -0500

    Added Enum Classes for selling manager

---
+++ src/D4m/NgnFeed/Ebay/Model/Enum/OrderRoleCode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raul
+ * Date: 2/24/14
+ * Time: 4:01 PM
+ */
+
+namespace D4m\NgnFeed\Ebay\Model\Enum;
+
+
+class OrderRoleCode
+{
+    const BUYER = "Buyer";
+    const SELLER = "Seller";
+    const CUSTOM_CODE = "CustomCode";
+} 
---
+++ src/D4m/NgnFeed/Ebay/Model/Enum/SortOrderCode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raul
+ * Date: 2/24/14
+ * Time: 11:19 AM
+ */
+
+namespace D4m\NgnFeed\Ebay\Model\Enum;
+
+
+class SortOrderCode
+{
+    const ASCENDING = "Ascending";
+    const DESCENDING = "Descending";
+    const CUSTOM_CODE = "CustomCode";
+} 
