bbf150798c3bd04820a3fd06d457a67daee3f22d
raul782 <raulrodriguez782@gmail.com>
2014-03-03 21:34:51 -0500

    Fixed extra space on OrderStatusCode

--- src/D4m/NgnFeed/Ebay/Model/Enum/OrderStatusCode.php
+++ src/D4m/NgnFeed/Ebay/Model/Enum/OrderStatusCode.php
@@ -9,15 +9,15 @@ namespace D4m\NgnFeed\Ebay\Model\Enum;
 
 class OrderStatusCode
 {
-    const ACTIVE = "Active ";
-    const INACTIVE = "Inactive ";
-    const COMPLETED = "Completed ";
-    const CANCELLED = "Cancelled ";
-    const SHIPPED = "Shipped ";
-    const DEFAULT_ = "Default ";
-    const AUTHENTICATED = "Authenticated ";
-    const IN_PROCESS = "InProcess ";
-    const INVALID = "Invalid ";
-    const CUSTOM_CODE = "CustomCode ";
+    const ACTIVE = "Active";
+    const INACTIVE = "Inactive";
+    const COMPLETED = "Completed";
+    const CANCELLED = "Cancelled";
+    const SHIPPED = "Shipped";
+    const DEFAULT_ = "Default";
+    const AUTHENTICATED = "Authenticated";
+    const IN_PROCESS = "InProcess";
+    const INVALID = "Invalid";
+    const CUSTOM_CODE = "CustomCode";
     const ALL = "All";
 }
